6e0083d787fd8454aa34fdbb9b3f422e92e8f5f8
vladhrechin-a11y <vladhrechin@gmail.com>
2026-08-16 19:18:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,23 @@
-const CACHE_NAME = "bezpeka-poruch-v10";
+const CACHE_NAME = "bezpeka-poruch-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-
   event.waitUntil(
-
-    caches.keys().then(names => {
-
-      return Promise.all(
-        names.map(name => caches.delete(name))
-      );
-
-    }).then(() => {
-
-      return self.clients.claim();
-
-    })
-
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
-
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
-
 });
